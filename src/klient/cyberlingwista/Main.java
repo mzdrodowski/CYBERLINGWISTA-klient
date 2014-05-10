7c7a88274fd8b9ee45aeeab2c0024e1a8cf9aa3d
Michał Zdrodowski <michal.zdrodowski@gmail.com>
2014-05-10 12:25:18 +0200

    To jest pierwszy commit ...  Na początek dodałem Ramkę logowania do interfejsu graficznego.

---
+++ src/klient/cyberlingwista/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package klient.cyberlingwista;
+
+import klient.cyberlingwista.ui.LoginFrame;
+
+/**
+ *
+ * @author Jan Kowalski
+ */
+public class Main {
+    public static void main(String []args){
+        LoginFrame lf = new LoginFrame();
+        lf.setVisible(true);
+    }
+}
